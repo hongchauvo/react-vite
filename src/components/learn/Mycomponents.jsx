1cb7ab8db03f3de13026a8279e6f5ff95e4058de
Võ Hồng Châu <hongchauvo@gmail.com>
2024-10-15 14:54:54 +0700

    video 29

---
+++ src/components/learn/Mycomponents.jsx
@@ -0,0 +1,7 @@
+const Mycomponent = () => {
+
+    return (<h3>Võ Hồng Châu Phòng TTPC ĐHQG-HCM</h3>
+
+    )
+}
+export default Mycomponent
---
+++ src/components/learn/Secondcomponent.jsx
@@ -0,0 +1,7 @@
+const Secondcomponent = () => {
+
+    return (
+        <div>Hỏi dân IT & Eric</div>
+    )
+}
+export default Secondcomponent
